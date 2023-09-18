547be8d75c72886663b799cc16bab9c7dada4d75
Michael090103 <michael.schuiling97@gmail.com>
2023-09-18 10:59:59 +0200

    Add form results view

--- php/classes/views/formresultsview.class.php
+++ php/classes/views/formresultsview.class.php
@@ -1,14 +1,20 @@
 <?php
 namespace views;
 
+use controllers\FormResultsController;
 use mvc\View;
 
 class FormResultsView extends View {
 
+    protected $controller;
+
+    public function __construct(FormResultsController $formResultsController)
+    {
+        $this->controller = $formResultsController;
+    }
+
     public function getHTML()
     {
-        return '
-            
-        ';
+        return $this->controller->test();
     }
 }
--- php/includes/default.inc.php
+++ php/includes/default.inc.php
@@ -1,6 +1,6 @@
 <?php
   //constants for including files
-  define("WEB_ROOT", DIRECTORY_SEPARATOR . "mvc" . DIRECTORY_SEPARATOR);
+  define("WEB_ROOT", DIRECTORY_SEPARATOR . "A4-PHP" . DIRECTORY_SEPARATOR);
   define("LOCAL_ROOT", $_SERVER['DOCUMENT_ROOT'] . WEB_ROOT);
 
   //function for loading class files inclusive namespaces
